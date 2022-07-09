bdd93ab8d7499d0d4f8257a76be2e945abdc114e
aniketgarje7 <garjeaniket7@gmail.com>
2022-07-09 18:01:13 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+package relevel;
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        int n = 9;
+        int i=n-1-1+n;
+        System.out.println(i%n);
+
+    }
+    
+}
+    
+  
+    
